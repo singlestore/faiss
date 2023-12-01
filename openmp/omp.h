580a59bf2990dce3a04f4ca4357031cb6fd18d5e
Cheng Chen <cchen@singlestore.com>
2023-12-01 00:26:19 -0800

    build(vector_index): Remove Some Openmp Asserts
    Summary:
    1. Enable some openmp functions that are used by HNSW;
    2. Upgrade FAISS to v1.7.4, and also cherry-pick https://github.com/facebookresearch/faiss/commit/82352dd4537afb8e8688f7332584e618fdd1d8e8 to enable nbits option for HNSWPQ;
    3. Upgrade MKL to the latest release 49656.
    
    Test Plan: .vector_index
    
    Reviewers: sasha, szupo, wuchun, zhou
    
    Subscribers: engineering-list
    
    Differential Revision: https://grizzly.internal.memcompute.com/D65831

--- openmp/omp.h
+++ openmp/omp.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 // https://openmp.org/wp-content/uploads/OpenMP4.0.0.pdf
 //
@@ -8,7 +10,7 @@
 // C.1 Example of the omp.h Header File
 //
 
-typedef void *omp_lock_t;
+typedef void* omp_lock_t;
 
 // A.1 C/C++ Stub Routines
 //
@@ -50,22 +52,64 @@ inline int omp_get_nested(void)
     return 0;
 }
 
-inline void omp_init_lock(omp_lock_t *arg)
+struct __omp_lock
 {
-    assert(false);
+    int lock;
+};
+
+enum
+{
+    UNLOCKED = -1,
+    INIT,
+    LOCKED
+};
+
+inline void omp_init_lock(omp_lock_t* arg)
+{
+    struct __omp_lock* lock = (struct __omp_lock*)arg;
+    lock->lock = UNLOCKED;
 }
 
-inline void omp_destroy_lock(omp_lock_t *arg)
+inline void omp_destroy_lock(omp_lock_t* arg)
 {
-    assert(false);
+    struct __omp_lock* lock = (struct __omp_lock*)arg;
+    lock->lock = INIT;
 }
 
-inline void omp_set_lock(omp_lock_t *arg)
+inline void omp_set_lock(omp_lock_t* arg)
 {
-    assert(false);
+    struct __omp_lock* lock = (struct __omp_lock*)arg;
+    if (lock->lock == UNLOCKED)
+    {
+        lock->lock = LOCKED;
+    }
+    else if (lock->lock == LOCKED)
+    {
+        fprintf(stderr, "error: deadlock in using lock variable\n");
+        exit(1);
+    }
+    else
+    {
+        fprintf(stderr, "error: lock not initialized\n");
+        exit(1);
+    }
 }
 
-inline void omp_unset_lock(omp_lock_t *arg)
+inline void omp_unset_lock(omp_lock_t* arg)
 {
-    assert(false);
+    struct __omp_lock* lock = (struct __omp_lock*)arg;
+    if (lock->lock == LOCKED)
+    {
+        lock->lock = UNLOCKED;
+    }
+    else if (lock->lock == UNLOCKED)
+    {
+        fprintf(stderr, "error: lock not set\n");
+        exit(1);
+    }
+    else
+    {
+        fprintf(stderr, "error: lock not initialized\n");
+        exit(1);
+    }
 }
